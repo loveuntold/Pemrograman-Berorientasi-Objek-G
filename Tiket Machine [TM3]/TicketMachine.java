440d6ae59f0a1ea1df2cc55fe4e07a4b32213d29
Ayesha Satrio <ayesha.satrio@gmail.com>
2024-09-17 16:13:19 +0700

    edit: menu.java and TicketMachine.java

---
+++ Tiket Machine [TM3]/TicketMachine.java
@@ -0,0 +1,48 @@
+public class TicketMachine {
+    private int price;
+    private int balance;
+    // private int total;
+
+    public TicketMachine(int ticketcost){
+        price = ticketcost;
+        balance = 0;
+        // total = 0;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public void insertMoney(int amount){
+        if(amount > 0){
+            balance += amount;
+        } else {
+            System.out.println("Insert a positive amount of money!");
+        }
+    }
+
+    public void printTicket(){
+        if(balance >= price){
+            System.out.println("\n##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            // total += price;
+            balance -= price;
+            System.out.println("Balance: " + balance + " cents.\n");
+        } else {
+            System.out.println("\nYou must insert at least: " + (price - balance) + " cents.\n");
+        }
+    }
+
+    public int refund(){
+        int amountToRefund = balance;
+        balance = 0;
+        return amountToRefund;
+    }
+}
---
+++ Tiket Machine [TM3]/menu.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class menu{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        TicketMachine tm = new TicketMachine(100);
+
+        System.out.println("\nWelcome to the Ticket Machine!");
+        System.out.println("Ticket price: " + tm.getPrice() + " cents.");
+        System.out.println("\nPlease insert money.");
+        
+        int choice = 0;
+        do{
+            System.out.println("1. Insert money");
+            System.out.println("2. Print ticket");
+            System.out.println("3. Refund");
+            System.out.println("4. Exit");
+            System.out.print("Choice: ");
+            choice = input.nextInt();
+            switch(choice){
+                case 1:
+                    System.out.print("Insert money: ");
+                    int money = input.nextInt();
+                    tm.insertMoney(money);
+                    System.out.println("\nBalance: " + tm.getBalance() + " cents.\n");
+
+                    break;
+                case 2:
+                    tm.printTicket();
+                    break;
+                case 3:
+                    System.out.println("\nRefunded: " + tm.refund() + " cents.\n");
+                    break;
+                case 4:
+                    System.out.println("\nGoodbye!\n");
+                    break;
+                default:
+                    System.out.println("\nInvalid choice!\n");
+            }
+        } while(choice != 4);
+    }
+    
+}
