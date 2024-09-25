b0206877bd722e63d7ef97b901b3c6e62018f2b7
Ayesha Satrio <ayesha.satrio@gmail.com>
2024-09-25 07:59:34 +0700

    update java

--- [TM4] Clock Display/Clock.java
+++ [TM4] Clock Display/Clock.java
@@ -1,3 +1,11 @@
-public class Clock {
-    
+import java.awt.*;  
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+
+public class Clock{
+    private JFrame frame;
+    private JLabel label;
+    private ClockDisplay clock;
 }
