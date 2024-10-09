16dfa4ddb5df0d98df9409188c500efd29c6c5af
Ayesha Satrio <ayesha.satrio@gmail.com>
2024-10-09 20:30:12 +0700

    update: tm6

---
+++ [TM6] Tech Support/InputReader.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class InputReader{
+    private Scanner reader;
+
+    public InputReader(){
+        reader = new Scanner(System.in);
+    }
+
+    public String getInput(){
+        System.out.print("> ");   
+        String inputLine = reader.nextLine();
+
+        return inputLine;
+    }
+}
